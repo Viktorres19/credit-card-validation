caf016be90ddbb66696369515c8ad8cd4beda1fc
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-08-26 12:56:01 +0300

    Added BE and combine it with FE

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', function(req, res) {
+  let error = {
+    cvvBox: '',
+    cardHolderName: '',
+    cardNumberBox: '',
+    expMonth: '',
+    expYear: ''
+  }
+  let data = req.body
+
+  const AE = /(^34|37)/.test(data.cardNumberBox)
+  if (AE && data.cardNumberBox.length !== 15
+    || !AE && data.cardNumberBox.length > 19 || data.cardNumberBox.length < 16) {
+    error.cardNumberBox = 'This field is filled incorrect'
+  }
+  if (AE && data.cvvBox.length !== 4 || !AE && data.cvvBox.length !== 3) {
+    error.cvvBox = 'This CVV is filled incorrect'
+  }
+  if (data.expMonth.length < 2 || data.expMonth === 1) {
+    data.expMonth = '0' + data.expMonth
+  } else if (data.expMonth === '00') {
+    error.expMonth = 'Your month looks wrong'
+  }
+  if (data.expYear.length < 2) {
+    error.expYear = 'This year looks wrong'
+  }
+
+  res.json({error})
 });
 
 module.exports = router;
